ac7b1add5b20ab7c1ffaa9c7f95c9ba9d610822c
Alex  Anderson <alexandersonbm@hotmail.com>
2019-07-13 18:38:40 -0300

    :white_check_mark: Refactor test name and exclude consulta test content

---
+++ test/br/uefs/ecomp/isnotpaxbahia/model/ConsultaTest.java
@@ -0,0 +1,28 @@
+package br.uefs.ecomp.isnotpaxbahia.model;
+
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+
+
+class ConsultaTeste {
+	
+	private Consulta consulta1;
+	
+	/**
+     * Este método é executado antes de cada teste de unidade (testes a seguir), 
+     * e serve para inicializar objetos que são utilizados nos testes.
+     */
+    @Before
+    public void setUp() throws Exception {
+
+//        this.consulta1 = new Consulta(proc1, 50);
+    }
+
+	@Test
+	public void testInitialization() {
+		
+	}
+
+
+}
