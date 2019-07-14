330f8703a32747baf583ae5a393975253843944c
zKorvus <32541183+zKorvus@users.noreply.github.com>
2019-07-14 19:33:46 -0300

    Update ConsultaTest.java

--- test/br/uefs/ecomp/isnotpaxbahia/model/ConsultaTest.java
+++ test/br/uefs/ecomp/isnotpaxbahia/model/ConsultaTest.java
@@ -10,19 +10,37 @@ class ConsultaTeste {
 	private Consulta consulta1;
 	
 	/**
-     * Este método é executado antes de cada teste de unidade (testes a seguir), 
-     * e serve para inicializar objetos que são utilizados nos testes.
+     * Este mÃ©todo Ã© executado antes de cada teste de unidade (testes a seguir), 
+     * e serve para inicializar objetos que sÃ£o utilizados nos testes.
      */
     @Before
     public void setUp() throws Exception {
 
-//        this.consulta1 = new Consulta(proc1, 50);
+    	this.proc1 = new Procedimento("hemograma","S10","Dr. Ricardo","Gertrudes");
+        this.consulta1 = new Consulta(proc1, 50,"Pediatra");
     }
 
 	@Test
 	public void testInitialization() {
-		
+		assertEquals(50,this.lote1.getQuantidade());
+		assertEquals("Pediatra",this.getEspecialidade());
 	}
 
+	@Test
+	public void testSetQuantity() {
+		assertEquals(50,this.lote1.getQuantidade());
+		this.lote1.setQuantity(10);
+		assertEquals(10,this.lote1.getQuantidade());
+	}
 
+	@Test
+	public void testDecreaseQuantity() {
+		assertEquals(50,this.lote1.getQuantidade());
+		this.lote1.decrease();
+		assertFalse(48,this.lote1.getQuantidade());
+		assertEquals(49,this.lote1.getQuantidade());
+		this.lote1.decrease();
+		assertEquals(48,this.lote1.getQuantidade());
+		assertEquals(0,this.lote1.getQuantidade());
+		assertNull(this.lote1.decrease());
 }
