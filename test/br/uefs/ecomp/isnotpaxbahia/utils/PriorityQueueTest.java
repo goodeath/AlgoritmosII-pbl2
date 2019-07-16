18a93c55ea9b789f2e3111e32e39974503fdde56
Alex  Anderson <alexandersonbm@hotmail.com>
2019-07-16 18:21:04 -0300

    :rocket: I don't have time to commit

--- test/br/uefs/ecomp/isnotpaxbahia/utils/PriorityQueueTest.java
+++ test/br/uefs/ecomp/isnotpaxbahia/utils/PriorityQueueTest.java
@@ -5,6 +5,10 @@ import org.junit.Before;
 import org.junit.jupiter.api.Test;
 
 import br.uefs.ecomp.isnotpaxbahia.utils.PriorityQueue;
+import br.uefs.ecomp.isnotpaxbahia.Model.Paciente;
+import br.uefs.ecomp.isnotpaxbahia.Model.Agendamento;
+import br.uefs.ecomp.isnotpaxbahia.Model.Lote;
+import br.uefs.ecomp.isnotpaxbahia.utils.IPrioridade;
 
 /**
 * a informação de cada nó desta linked list é considerada como a prioridade no problema
@@ -12,12 +16,14 @@ import br.uefs.ecomp.isnotpaxbahia.utils.PriorityQueue;
 
 public class QueueTeste {
 
-	private PriorityQueue <Agendamento> queue;	
-
+	private PriorityQueue <IPrioridade> queue;	
+	private Lote l1, l2, l3, l4, l5;
+	private Agendamento a1, a2, a3, a4, a5;
+	private Paciente p1, p2, p3, p4, p5;
 
 @Before
 public void setUp(){
-	queue = new MyPriorityQueue<Agendamento>();
+	queue = new MyPriorityQueue<IPrioridade>();
 	
 	Lote l1 = new Lote(50,"hemograma");
 	Paciente p1 = new Paciente("marcio","M","Rua alvorada","7699990000","01/10/1990");
@@ -46,111 +52,49 @@ public void setUp(){
 * Testa se o elemento de maior prioridade está sendo retornado
 */
 @Test
-public void testMax(){
+public void testAdd(){
 	queue.enqueue(a1);
-	queue.enqueue(a2);
-	assertEquals(a2,queue.max());
-	queue.enqueue(a3);
-	assertEquals(a3,queue.max());
+	assertTrue(queue.get(0).equals(a1));
+	assertEquals(queue.get(0).getPrioridade(),1);
+	assertEquals(queue.size(),1);
+	
+	queue.enqueue(a2,Agendamento.PRIORIDADE_GRAVIDA);
+	assertTrue(queue.get(0).equals(a2));
+	assertEquals(queue.get(0).getPrioridade(),2);
+	assertEquals(queue.size(),2);
+	
+	queue.enqueue(a3,Agendamento.PRIORIDADE_IDOSO);
+	assertTrue(queue.get(0).equals(a3));
+	assertEquals(queue.get(0).getPrioridade(),3);
+	assertEquals(queue.size(),3);
+	
+	queue.enqueue(a4,Agendamento.PRIORIDADE_DEFICIENTE);
+	assertTrue(queue.get(0).equals(a4));
+	assertEquals(queue.get(0).getPrioridade(),4);
+	assertEquals(queue.size(),4);
+	
+	queue.enqueue(a5,Agendamento.PRIORIDADE_MUITO_IDOSO);
+	assertTrue(queue.get(0).equals(a5));
+	assertEquals(queue.get(0).getPrioridade(),5);
+	assertEquals(queue.size(),5);
+	
+	queue = new MyPriorityQueue<IPrioridade>();
+	
+	queue.enqueue(a5,Agendamento.PRIORIDADE_MUITO_IDOSO);
+	queue.enqueue(a4,Agendamento.PRIORIDADE_MUITO_IDOSO);
+	assertTrue(queue.get(0).equals(a5));
+	assertTrue(queue.get(1).equals(a4));
+	queue.enqueue(a3,Agendamento.PRIORIDADE_IDOSO);
+	assertTrue(queue.get(2).equals(a3));
+	queue.enqueue(a2,Agendamento.PRIORIDADE_IDOSO);
+	assertTrue(queue.get(2).equals(a3));
+	assertTrue(queue.get(3).equals(a2));
 }
 
-/**
-*  Teste que verifica se os pacientes de prioridade 5 estão sendo inseridos no começo
-*/
-@Test
-public void insertP5(){
-	queue.enqueue(a1);
-	queue.enqueue(a2);
-	queue.enqueue(a2);
-	queue.enqueue(a3);
-	assertEquals(a3,queue.max());
-	queue.enqueue_p5();
-	assertEquals(,queue.max());
-	assertEquals(5,queue.first());
-	queue.enqueue(3);
-	queue.enqueue_p5(5);
-	assertEquals(5,queue.first());
-	queue.dequeue();
-	assertEquals(5,queue.first());
-}
-	
-	
+ 
 /**
 * Teste unitário que verifica inserção dos elementos na fila
 */
-@Test
-public void testEnqueue(){
-	queue.enqueue(a1);
-	assertEquals(a1, queue.peek());
-	assertEquals(a1, queue.max());    //max retorna elemento de maior prioridade
-	queue.enqueue(a2);
-	assertEquals(a2, queue.max());
-	queue.enqueue(a3);
-	assertEquals(a2, queue.max());
-}
-	
-/**
-*Testa insercao de dois elementos, sendo o segundo de maior prioridade, portanto o primeiro a ser removido
-*/
-@Test
-public void testinsert2_removemaiorprioridade(){
-	queue.enqueue(a1);
-	queue.enqueue(a2);
-//	assertEquals(a2,queue.first());
-	assertEquals(a2,queue.dequeue());
-}
-	
-@Test
-public void testinsert3_RemoveMaiorPrioridade(){
-	queue.enqueue(a1);
-	queue.enqueue(a3);
-	queue.enqueue(a2);
-//	assertEquals(a2,queue.first());
-	assertEquals(a2,queue.dequeue());
-}
-	
-@Test	
-public void testInsert4_RemoveMaiorPrioridade(){
-	queue.enqueue(a1);
-	queue.enqueue(a3);
-	queue.enqueue(a5);
-	queue.enqueue(a4);
-//	assertEquals(a4,queue.first());
-	assertEquals(a4,queue.dequeue());
-}
-	
-@Test	
-public void testInsert5_RemoveMaiorPrioridade(){
-	queue.enqueue(a1);
-	queue.enqueue(a4);
-	queue.enqueue(a3);
-	queue.enqueue(a2);
-	queue.enqueue(a5);
-	assertEquals(a5,queue.dequeue());
-}
-	
-@Test
-public void testisEmpty(){
-	assertTrue(queue.isEmpty());
-	queue.enqueue(a1);
-	assertFalse(queue.isEmpty());
-	queue.dequeue();
-	assertTrue(queue.isEmpty());
-	queue.enqueue(a2);
-	queue.enqueue(a3);
-	assertFalse(queue.isEmpty());
-	queue.dequeue();
-	assertFalse(queue.isEmpty());
-	queue.dequeue();
-	assertFalse(queue.isEmpty());
-	queue.dequeue();
-	assertTrue(queue.isEmpty());
-}
-
-/**
-/* Teste unitário que verifica a remoção dos elementos da fila
-*/
-
 @Test
 public void testRemove(){
 	queue.enqueue(a1);
@@ -171,5 +115,25 @@ public void testRemove(){
 	assertEquals(0,queue.size());
 	assertNull(queue.dequeue());
 }
+	
+
+@Test
+public void testIsEmpty(){
+	assertTrue(queue.isEmpty());
+	queue.enqueue(a1);
+	assertFalse(queue.isEmpty());
+	queue.dequeue();
+	assertTrue(queue.isEmpty());
+	queue.enqueue(a2);
+	queue.enqueue(a3);
+	assertFalse(queue.isEmpty());
+	queue.dequeue();
+	assertFalse(queue.isEmpty());
+	queue.dequeue();
+	assertFalse(queue.isEmpty());
+	queue.dequeue();
+	assertTrue(queue.isEmpty());
+}
+ 
 
 }
