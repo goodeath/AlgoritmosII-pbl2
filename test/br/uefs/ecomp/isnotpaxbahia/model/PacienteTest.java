684153adc9a09edc04209227fdf9c99edff9242b
Alex Anderson <alexandersonbm@hotmail.com>
2019-07-21 00:07:02 -0300

    :bug: Fix pacient test

--- test/br/uefs/ecomp/isnotpaxbahia/model/PacienteTest.java
+++ test/br/uefs/ecomp/isnotpaxbahia/model/PacienteTest.java
@@ -2,14 +2,14 @@ package br.uefs.ecomp.isnotpaxbahia.model;
 
 import static org.junit.Assert.*;
 import org.junit.Before;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
 import br.uefs.ecomp.isnotpaxbahia.model.Paciente;
 
 import java.util.Calendar;
 import java.util.Date;
 
-class PacienteTest {
+public class PacienteTest {
 	
 	private Paciente paciente1;
 	
@@ -18,26 +18,27 @@ class PacienteTest {
      * e serve para inicializar objetos que são utilizados nos testes.
      */
     @Before
-    public void setUp() throws Exception {
-        this.paciente1 = new Paciente("Tod Guten", "M", "Rua do Hittler", "7599534432","12/06/1990");
+    public void setUp() {
+        paciente1 = new Paciente("Tod Guten", 'M', "Rua do Hittler", "7599534432","12/06/1990");
     }
 
 	@Test
 	public void testInitialization() {
-		assertEquals("Tod Guten",this.paciente1.getNome());
-		assertEquals("7599534432",this.paciente1.getTelefone());
-		assertEquals("Rua do Hittler",this.paciente1.getEndereco());
-		assertEquals("M",this.paciente1.getGender());
-		assertEquals(Calendar.YEAR-1990, this.paciente1.getIdade());
+		Calendar today = Calendar.getInstance();
+		assertEquals("Tod Guten",paciente1.getNome());
+		assertEquals("7599534432",paciente1.getTelefone());
+		assertEquals("Rua do Hittler",paciente1.getEndereco());
+		assertEquals('M',paciente1.getGender());
+		assertEquals((today.get(Calendar.YEAR))-1990, paciente1.getIdade());
 	}
 	
 	@Test
 	public void testEquals() {
-		Paciente paciente2 = new Paciente("Karla Péres", "F", "Rua do Hittler", "7599534432","12/06/1990");
+		Paciente paciente2 = new Paciente("Karla Péres", 'F', "Rua do Hittler", "7599534432","12/06/1990");
 		assertFalse(this.paciente1.equals(paciente2));
-		Paciente paciente3 = new Paciente("Tod Guten", "M","Rua do Hittler","1194663520","12/06/1990");
+		Paciente paciente3 = new Paciente("Tod Guten", 'M',"Rua do Hittler","1194663520","12/06/1990");
 		assertFalse(this.paciente1.equals(paciente3));
-		Paciente paciente4 = new Paciente("Tod Guten", "M","Rua do Hittler","7599534432","12/06/1990");
+		Paciente paciente4 = new Paciente("Tod Guten", 'M',"Rua do Hittler","7599534432","12/06/1990");
 		assertTrue(this.paciente1.equals(paciente4));
 	}
 
