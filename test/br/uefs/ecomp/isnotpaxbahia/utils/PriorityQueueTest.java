e594d3103e21a8e59f6f514febd725b171c8a91f
zKorvus <32541183+zKorvus@users.noreply.github.com>
2019-07-21 23:40:06 -0300

    Update PriorityQueueTest.java

--- test/br/uefs/ecomp/isnotpaxbahia/utils/PriorityQueueTest.java
+++ test/br/uefs/ecomp/isnotpaxbahia/utils/PriorityQueueTest.java
@@ -26,23 +26,23 @@ public class QueueTest {
 		queue = new PriorityQueue<IPrioridade>();
 		
 		Lote l1 = new Lote(50,"hemograma");
-		Paciente p1 = new Paciente("marcio","M","Rua alvorada","7699990000","01/10/1990");
+		Paciente p1 = new Paciente("marcio","M","Rua alvorada","7699990000","01/10/1990", "54597533508");
 		Agendamento a1 = new Agendamento(Lote l1,Paciente p1);
 		
 		Lote l2 = new Lote(50,"consulta geral");
-		Paciente p2 = new Paciente("jose","M","Rua nova alvorada","7699990001","01/10/1910");
+		Paciente p2 = new Paciente("jose","M","Rua nova alvorada","7699990001","01/10/1910", "85803939579");
 		Agendamento a2 = new Agendamento(Lote l2,Paciente p2);
 		
 		Lote l3 = new Lote(50,"raiox");
-		Paciente p3 = new Paciente("florentina","F","Avenida alvorada","7699990002","01/10/1987");
+		Paciente p3 = new Paciente("florentina","F","Avenida alvorada","7699990002","01/10/1987", "66629301560");
 		Agendamento a3 = new Agendamento(Lote l3,Paciente p3);
 		
 		Lote l4 = new Lote(50,"ressonancia magnetica");
-		Paciente p4 = new Paciente("josefina","F","Avenida afonso pena","7699990003","04/04/1977");
+		Paciente p4 = new Paciente("josefina","F","Avenida afonso pena","7699990003","04/04/1977", "62577395515");
 		Agendamento a4 = new Agendamento(Lote l4,Paciente p4);
 		
 		Lote l5 = new Lote(50,"ultrassom");
-		Paciente p5 = new Paciente("josefa","F","Favela do alemao","7699990004","04/04/1958");
+		Paciente p5 = new Paciente("josefa","F","Favela do alemao","7699990004","04/04/1958", "71702517527");
 		Agendamento a5= new Agendamento(Lote l5,Paciente p5);
 		
 		
@@ -78,7 +78,7 @@ public class QueueTest {
 		assertEquals(queue.get(0).getPrioridade(),5);
 		assertEquals(queue.size(),5);
 		
-		queue = new MyPriorityQueue<IPrioridade>();
+		queue = new PriorityQueue<IPrioridade>();
 		
 		queue.enqueue(a5,Agendamento.PRIORIDADE_MUITO_IDOSO);
 		queue.enqueue(a4,Agendamento.PRIORIDADE_MUITO_IDOSO);
