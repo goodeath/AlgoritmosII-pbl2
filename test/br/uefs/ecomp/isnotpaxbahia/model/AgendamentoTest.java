4e70ae0faa0f0312ec069f4d83f69c2f495e625e
zKorvus <32541183+zKorvus@users.noreply.github.com>
2019-07-21 21:33:21 -0300

    AgendamentoTest Updated

--- test/br/uefs/ecomp/isnotpaxbahia/model/AgendamentoTest.java
+++ test/br/uefs/ecomp/isnotpaxbahia/model/AgendamentoTest.java
@@ -26,8 +26,8 @@ public class AgendamentoTest {
     @Before
     public void setUp() {
     	this.paciente1 = new Paciente("Tod Guten", 'M', "Rua Alciano", "7599534432", "12/06/1989", "43292502500");
-    	this.proc1 = new Exame("Endoscopia", "S1", "Dr. Ricardo", "Gertrudes");
-    	this.proc2 = new Consulta("Clinico Geral", "S1", "Dr. Ricardo", "Gertrudes");
+    	this.proc1 = new Exame("Endoscopia", "S1", "Dr. Ricardo", "Gertrudes", "419247");
+    	this.proc2 = new Consulta("Clinico Geral", "S1", "Dr. Ricardo", "Gertrudes", "145367");
         this.lote1 = new Lote(proc1, 50);
     	this.lote2 = new Lote(proc2, 50);
     }
