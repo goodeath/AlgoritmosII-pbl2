81c9eef50c4cb98bb63945dc5aa4bd4a6c31cd17
zKorvus <32541183+zKorvus@users.noreply.github.com>
2019-07-23 01:22:54 -0300

    Update AgendamentoTest.java

--- test/br/uefs/ecomp/isnotpaxbahia/model/AgendamentoTest.java
+++ test/br/uefs/ecomp/isnotpaxbahia/model/AgendamentoTest.java
@@ -27,7 +27,7 @@ public class AgendamentoTest {
     public void setUp() {
     	this.paciente1 = new Paciente("Tod Guten", 'M', "Rua Alciano", "7599534432", "12/06/1989", "43292502500");
     	this.proc1 = new Exame("Endoscopia", "S1", "Dr. Ricardo", "Gertrudes", "419247");
-    	this.proc2 = new Consulta("Clinico Geral", "S1", "Dr. Ricardo", "Gertrudes", "145367");
+    	this.proc2 = new Consulta("Clinico Geral", "S1", "Dr. Ricardo", "Gertrudes", "145367", "Urologia");
         this.lote1 = new Lote(proc1, 50);
     	this.lote2 = new Lote(proc2, 50);
     }
@@ -35,14 +35,14 @@ public class AgendamentoTest {
 	@Test
 	public void testInitialization() {
 		Agendamento agendamento = new Agendamento(this.lote1,this.paciente1);
-		assertEquals(this.lote1.getQuantity(),49);
-		assertEquals(this.lote2.getQuantity(),50);
+		assertEquals(this.lote1.getQuantidade(),49);
+		assertEquals(this.lote2.getQuantidade(),50);
 		assertTrue(agendamento.getPaciente().equals(this.paciente1));
 		assertTrue(agendamento.getProcedimento().equals(proc1));
 		Agendamento agendamento2 = new Agendamento(this.lote2,this.paciente1);
-		assertEquals(this.lote2.getQuantity(),49);
+		assertEquals(this.lote2.getQuantidade(),49);
 		Agendamento agendamento3 = new Agendamento(this.lote2,this.paciente1);
-		assertEquals(this.lote2.getQuantity(),48);
+		assertEquals(this.lote2.getQuantidade(),48);
 	}
 	
 	@Test
